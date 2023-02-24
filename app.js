8a02e6e5d32d1c640eb2d05067a8f0c6e399b23c
subhajit mallick <subhajit.mallick.7370@gmail.com>
2023-02-24 22:38:45 +0530

    Did code cleanup and added dynamic pretext

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 const express = require("express");
-const bosyParser = require("body-parser");
-const today = require(__dirname + "/date.js");
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const _ = require('lodash');
 
-let isFirst = true;
-
+const today = require(__dirname + "/date.js");
 const date = today.day();
-
 
 const { Schema } = mongoose;
 mongoose.connect('mongodb://localhost:27017/todoeyDB');
 const itemSchema = new Schema({
     text: String,
-    // chk: Boolean
 });
 const Item = mongoose.model('Item', itemSchema);
+const defaultItems = [];
 
-const item1 = new Item({
-    text: "Welcome to Todoey",
-    // chk: false
+const Prereq = mongoose.model('Prereq', 
+new Schema({ pretext: [String]}), 'prereq');
+Prereq.findById('63f8e85b31906cf1191f4ba7',(err,preDoc)=>{
+    if(err) console.log(err);
+    else{
+        preDoc.pretext.forEach((ptxt)=>{
+            const item = new Item({
+                text: ptxt,
+            });
+            defaultItems.push(item);
+        });
+    }
 });
-const item2 = new Item({
-    text: "Hit + to add new Item",
-    // chk: false
-});
-const item3 = new Item({
-    text: "<-- Hit this to delete an Item",
-    // chk: false
-});
-const defaultItems = [item1, item2, item3];
 
 const listSchema = new Schema({
     name: String,
@@ -38,13 +35,13 @@ const listSchema = new Schema({
 
 const List = mongoose.model('List', listSchema);
 
-
+//Express-Initialization
 const app = express();
-app.use(bosyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-
+//Get-MainRoute
 app.get('/', (req, res) => {
 
     List.findOne({ name: 'today' }, (err, foundList) => {
@@ -61,22 +58,9 @@ app.get('/', (req, res) => {
             res.redirect('/');
         }
     });
-    // Item.find({}, (err, itemList) => {
-
-    //     if (itemList.length === 0 && isFirst === true) {
-    //         isFirst = false;
-    //         Item.insertMany(defaultItems, (err) => {
-    //             if (err) console.log(err)
-    //             else console.log('Default Items Inserted');
-    //             // res.redirect("/");
-    //         });
-    //     }
-    //     // else {
-    //     res.render('todolist', { listTitle: date, items: itemList });
-    //     // }
-    // });
 });
 
+//Get-CustomList
 app.get('/:customList', (req, res) => {
     const listName = _.capitalize(req.params.customList);
 
@@ -96,6 +80,7 @@ app.get('/:customList', (req, res) => {
     });
 });
 
+//Create Entries/Items
 app.post('/', (req, res) => {
     const item = req.body.itemText;
     let listName = req.body.listName;
@@ -104,38 +89,26 @@ app.post('/', (req, res) => {
     if (item) {
         const newItem = new Item({
             text: item,
-            // chk: false
         });
 
-        // if (listName === date) {
-        //     newItem.save();
-        //     res.redirect('/');
-        // } else {
         List.findOne({ name: listName }, (err, foundList) => {
             foundList.items.push(newItem);
             foundList.save();
             if (listName === 'today') listName = '';
             res.redirect('/' + listName);
         });
-        // }
     } else {
         if (listName === 'today') listName = '';
         res.redirect('/' + listName);
     }
 });
 
+//Delete Entities
 app.post("/chk", (req, res) => {
     const checkedItemID = req.body.checkbox;
     let listName = req.body.listName;
     if (listName === date) listName = 'today';
 
-    // if (listName === date) {
-    //     Item.findByIdAndRemove(req.body.checkbox, (err) => {
-    //         if (err) console.log(err)
-    //         else console.log('Item Deleted');
-    //     });
-    //     res.redirect("/");
-    // } else {
     List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemID } } }, (err, foundList) => {
         if (listName === 'today') listName = '';
 
@@ -143,20 +116,6 @@ app.post("/chk", (req, res) => {
             res.redirect('/' + listName);
         } else console.log(err);
     });
-    // }
-
 });
 
-// app.post('/r', (req, res) => {
-//     items = [];
-//     res.redirect("/");
-// });
-
 app.listen(process.env.PORT || 3000, () => { console.log("server started...") });
-
-//TODO:
-// 1) Remove error -> when all items are deleted,
-// default items are again inserted.
-// soln- Create a list for today like other lists (home, school)
-
-//2) Reset Functionality
